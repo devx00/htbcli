d2c9a271e5338afdc9de2ca792259bca497d7a8f
Zach Hanson <zachhanson94@gmail.com>
2019-11-14 00:14:32 -0800

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='htbcli',
-    version='1.1.0',
+    version='1.0.1',
 
     description='Hack the Box CLI',
     long_description=long_description,
