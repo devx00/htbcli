db2efab68dd72765c7e7ac963dd6e0e9b12de86c
Zach Hanson <zachhanson94@gmail.com>
2021-01-31 07:26:15 -0800

    Bump version: 1.1.4 → 1.1.5

--- htbcli/__init__.py
+++ htbcli/__init__.py
@@ -5,7 +5,7 @@ import requests
 from htb import HTB, HTBAPIError
 
 
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 
 class HTBCLIError(HTBAPIError):
     """Raised when API fails"""
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='htbcli',
-    version='1.1.4',
+    version='1.1.5',
 
     description='Hack the Box CLI',
     long_description=long_description,
